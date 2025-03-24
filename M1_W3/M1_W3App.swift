e8a8847d94ffa801c36dcc287be13c179fd52c67
Tahani50 <tahanyayman4@gmail.com>
2025-03-24 10:46:27 +0300

    Initial Commit

---
+++ M1_W3/M1_W3App.swift
@@ -0,0 +1,17 @@
+//
+//  M1_W3App.swift
+//  M1_W3
+//
+//  Created by Taibah Valley Academy on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct M1_W3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
